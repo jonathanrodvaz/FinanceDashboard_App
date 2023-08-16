447a4e37d449bd2e5eac8ea11fb30462c7d3dee4
jonathanrodvaz <jonathanrodvazquez@gmail.com>
2023-08-16 19:10:13 +0200

    Added Models and Routes

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
-
+import kpiRoutes from "./routes/kip.js";
 
 /*CONFIG*/
 dotenv.config()
@@ -19,6 +19,72 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
 
+/* ROUTES */
+app.use("/kpi", kpiRoutes);
+
 console.log("hello there!");
 
 /*MONGOOSE SETUP*/
+const PORT = process.env.PORT || 9000;
+mongoose
+.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(async () => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`))
+})
+.catch((error) => console.log(`${error} did not connect`));
+
+
+//Igor's Code
+// import express from "express"
+// import bodyParser from "body-parser"
+// import mongoose from "mongoose"
+// import cors from "cors"
+// import dotenv from "dotenv"
+// import helmet from "helmet"
+// import morgan from "morgan"
+// // import kpiRoutes from "./routes/kpi.js"
+// // import productRoutes from "./routes/product.js"
+// // import transactionRoutes from "./routes/transaction.js"
+// // import KPI from "./models/KPI.js"
+// // import Product from "./models/Product.js"
+// // import Transaction from "./models/Transaction.js"
+// // import { kpis, products, transactions } from "./data/data.js"
+
+// // CONFIGURATIONS
+// dotenv.config()
+// const app = express()
+// app.use(express.json())
+// app.use(helmet())
+// app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }))
+// app.use(morgan("common"))
+// app.use(bodyParser.json())
+// app.use(bodyParser.urlencoded({ extended: "false" }))
+// app.use(cors())
+
+// // ROUTES 
+// // app.use("/kpi", kpiRoutes)
+// // app.use("/product", productRoutes)
+// // app.use("/transaction", transactionRoutes)
+
+// // MONGOOSE SETUP
+// const PORT = process.env.PORT || 9000
+
+// mongoose
+//     .connect(process.env.MONGO_URL, {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true,
+//     })
+//     .then(async () => {
+//         app.listen(PORT, () => console.log(`Server Port: ${PORT}`))
+
+//         /* ADD DATA ONE TIME ONLY OR AS NEEDED */
+//         // ADD DATA TO MONGDB
+//         // await mongoose.connection.db.dropDatabase();
+//         // KPI.insertMany(kpis);
+//         // Product.insertMany(products);
+//         // Transaction.insertMany(transactions);
+//     })
+//     .catch((error) => console.log("Conection error: ", error))
