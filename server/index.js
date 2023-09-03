538ea08c7b840c78c3b142d5d8525c45c523e3ae
jonathanrodvaz <jonathanrodvazquez@gmail.com>
2023-09-03 19:46:52 +0200

    Reworking cors at index

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.use(bodyParser.urlencoded({ extended: "false" }))
 app.use(cors())
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://finance-dashboard-app-xsot-52x0zis86-jonathanrodvaz.vercel.app');
+    res.header('Access-Control-Allow-Origin', 'https://finance-dashboard-app-ztdf.vercel.app/');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
 })
